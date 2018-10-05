4c1a5178aba9cf813470a0cce6fed0e61ff62ccc
James Hopkins <jameshopkins0709@gmail.com>
2018-10-05 23:17:45 +0100

    Add basic Github API integration

--- src/Main.elm
+++ src/Main.elm
@@ -1,36 +1,108 @@
 module Main exposing (main)
 
 import Browser exposing (Document, document)
-import Html exposing (..)
-import Json.Decode as Decode exposing (Value)
+import Html exposing (Html, button, form, h1, input, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput, onSubmit)
+import Http
+import Iso8601 as CrappyDateString
+import Json.Decode as Decode exposing (Decoder, Value)
+import Time exposing (Posix)
 
 
 type alias Model =
-    { count : Int
+    { query : String
+    }
+
+
+type alias Owner =
+    { name : String
+    , avatar : String
+    }
+
+
+type alias Project =
+    { name : String
+    , owner : Owner
+    , url : String
+    , lastUpdated : Posix
+    , description : Maybe String
     }
 
 
 type Msg
-    = Count
+    = SubmitQuery
+    | MutateQuery String
+    | SearchResult (Result Http.Error (List Project))
 
 
 init : Value -> ( Model, Cmd msg )
 init _ =
-    ( { count = 0 }, Cmd.none )
+    ( Model "", Cmd.none )
 
 
-update : Msg -> Model -> ( Model, Cmd msg )
+submitQuery : String -> Cmd Msg
+submitQuery query =
+    let
+        decoder =
+            Decode.field "items" (Decode.list projectDecoder)
+
+        request =
+            Http.get
+                ("https://api.github.com/search/repositories?q="
+                    ++ query
+                )
+                decoder
+    in
+    Http.send SearchResult request
+
+
+ownerDecoder : Decoder Owner
+ownerDecoder =
+    Decode.map2 Owner
+        (Decode.field "login" Decode.string)
+        (Decode.field "avatar_url" Decode.string)
+
+
+projectDecoder : Decoder Project
+projectDecoder =
+    Decode.map5 Project
+        (Decode.field "name" Decode.string)
+        (Decode.field "owner" ownerDecoder)
+        (Decode.field "url" Decode.string)
+        (Decode.field "updated_at" CrappyDateString.decoder)
+        (Decode.field "description" (Decode.nullable Decode.string))
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Count ->
-            ( { count = model.count + 1 }, Cmd.none )
+        MutateQuery val ->
+            ( { query = val }, Cmd.none )
+
+        SubmitQuery ->
+            ( model, submitQuery model.query )
+
+        SearchResult res ->
+            Debug.log (Debug.toString res)
+                ( model, Cmd.none )
+
+
+search : Html Msg
+search =
+    Html.form [ onSubmit SubmitQuery ]
+        [ input [ type_ "text", onInput MutateQuery ] []
+        , button [ type_ "submit" ] [ text "Search!" ]
+        ]
 
 
 view : Model -> Document Msg
 view model =
-    { title = "WAHEY IT WORKS"
+    { title = "Github Repository Search"
     , body =
-        [ Html.h1 [] [ text "nice one!" ]
+        [ h1 []
+            [ text "Search Github Projects!" ]
+        , search
         ]
     }
 
